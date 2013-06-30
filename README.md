c00122af03b6181c12d5b0380011156b790c5569
Damiano Stoffie <fffffffu@gmail.com>
2013-06-30 22:56:14 +0200

    Faster routine

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ javascript-maze
 ===============
 
 Render a maze inside a canvas. The program could be improved: it uses a kind-of double buffering while it's not needed.
-Released into the pubblic domain.
+Released into the public domain.
--- maze.js
+++ maze.js
@@ -4,161 +4,129 @@ var org = org || {};
 org.stoffie = org.stoffie || {};
 org.stoffie.maze = org.stoffie.maze || {};
 
-//(function () {
-console.log('hi maze.js');
-	var maze = org.stoffie.maze;
-	maze.CELL_SIZE = 50; // How many pixels is big a cell?
-	maze.DIRECTIONS = ['up','down','left','right'];
-	/* A wall is done of concrete.
-	 */
-	maze.Wall = function () {
-		this.exists = true; // Should the wall be drawn?
-	}
-	
-	/* A cell is surrounded by walls.
-	 */
-	maze.Cell = function () {
-		this.visited = false; // Has been the cell visited?
-		this.neighbors = {}; // At most 4: up down left right.
-		this.neighbor_walls = {};
-	};
-	maze.Cell.prototype.unvisited_neighbors = function () {
-		var map = {};
-		for (var key in this.neighbors) {
-			if (!this.neighbors[key].visited) {
-				map[key] = this.neighbors[key];
-			}
-		}
-		return map;
-	}
-	
-	/* Data structure that keeps track of the cells.
-	 */
-	maze.Maze = function (rows, columns) {
-		this.complete = false;
-		this.rows = rows;
-		this.columns = columns;
-		this.cells = [];
-		this.cell_stack = [];
-		for (var i = 0; i < rows; i++) {
-			this.cells[i] = [];
-			for (var j = 0; j < columns; j++) {
-				this.cells[i][j] = new maze.Cell();
-			}
-		}
+(function () {
 
-		for (var i = 0; i < this.rows -1; i++) {
-			for (j = 0; j < this.columns; j++) {
-//console.log('vertical wall i:' + i + ' j:' + j);
-				var wall = new maze.Wall();
-				var cell_above = this.cells[i][j];
-				var cell_down = this.cells[i + 1][j];
-				cell_above.neighbors['down'] = cell_down;
-				cell_above.neighbor_walls['down'] = wall;
-				cell_down.neighbors['up'] = cell_above;
-				cell_down.neighbor_walls['up'] = wall;
-			}
+var maze = org.stoffie.maze;
+maze.CELL_SIZE = 25; // How many pixels is big a cell?
+maze.BACKGROUND = '#000000';
+maze.FOREGROUND = '#FFFFFF';
+
+/* A wall is done of concrete.
+ */
+maze.Wall = function (x, y) {
+	this.exists = true; // Should the wall be drawn? NOT USED
+	this.x = x;
+	this.y = y;
+}
+
+/* A cell is surrounded by walls.
+ */
+maze.Cell = function (x, y) {
+	this.visited = false; // Has been the cell visited?
+	this.neighbors = {}; // At most 4: up down left right.
+	this.neighbor_walls = {};
+	this.x = x;
+	this.y = y;
+};
+
+maze.Cell.prototype.unvisited_neighbors = function () {
+	var map = {};
+	for (var key in this.neighbors) {
+		if (!this.neighbors[key].visited) {
+			map[key] = this.neighbors[key];
 		}
-		for (var i = 0; i < this.rows; i++) {
-			for (j = 0; j < this.columns -1; j++) {
-//console.log('horizontal wall i:' + i + ' j:' + j);
-				var wall = new maze.Wall();
-				var cell_left = this.cells[i][j];
-				var cell_right = this.cells[i][j + 1];
-				cell_left.neighbors['right'] = cell_right;
-				cell_left.neighbor_walls['right'] = wall;
-				cell_right.neighbors['left'] = cell_left;
-				cell_right.neighbor_walls['left'] = wall;
-			}
+	}
+	return map;
+}
+
+/* Data structure that keeps track of the cells.
+ */
+maze.Canvas = function (element, cell_size = maze.CELL_SIZE) {
+	this.ctx = element.getContext("2d");
+	this.cell_size = cell_size
+	this.complete = false;
+	this.cells = [];
+	this.cell_stack = [];
+	columns = Math.floor(element.width / cell_size);
+	rows = Math.floor(element.height / cell_size);
+	if (columns % 2 == 0) columns--;
+	if (rows % 2 == 0) rows--;
+	this.x_span = (element.width - cell_size * columns) / 2;
+	this.y_span = (element.height - cell_size * rows) / 2;
+	this.rows = (rows -1)/2;
+	this.columns = (columns -1)/2;
+	for (var i = 0; i < rows; i++) {
+		this.cells[i] = [];
+		for (var j = 0; j < columns; j++) {
+			this.cells[i][j] = new maze.Cell(j * 2, i * 2);
 		}
-		this.current_cell = this.cells[0][0];
-		this.cell_stack.push(this.cells[0][0]);
-	};
-	maze.Maze.prototype.continue_generation = function () {
-//console.log('continue_generation');
-		var current_cell = this.cell_stack[this.cell_stack.length -1];
-		current_cell.visited = true;
-		while (this.cell_stack.length > 0) {
-			var neighbors = current_cell.unvisited_neighbors();
-			var neighbors_count = 0;
-			var neighbors_list = [];
-			for (var key in neighbors) {
-				neighbors_count++;
-				neighbors_list.push(key);
-			}
-			if (neighbors_count > 0) {
-				// Chose a random neighbor and insert it into the stack.
-				var direction = neighbors_list[Math.floor(Math.random() * neighbors_count)];
-				current_cell.neighbor_walls[direction].exists = false;
-				this.cell_stack.push(current_cell.neighbors[direction]);
-				break;
-			} else {
-				this.cell_stack.pop();
-				current_cell = this.cell_stack[this.cell_stack.length -1];
-			}
+	}
+
+	for (var i = 0; i < this.rows -1; i++) {
+		for (j = 0; j < this.columns; j++) {
+			var wall = new maze.Wall(j * 2, i * 2 + 1);
+			var cell_above = this.cells[i][j];
+			var cell_down = this.cells[i + 1][j];
+			cell_above.neighbors['down'] = cell_down;
+			cell_above.neighbor_walls['down'] = wall;
+			cell_down.neighbors['up'] = cell_above;
+			cell_down.neighbor_walls['up'] = wall;
 		}
-		if (this.cell_stack.length == 0) {
-			this.complete = true;
+	}
+	for (var i = 0; i < this.rows; i++) {
+		for (j = 0; j < this.columns -1; j++) {
+			var wall = new maze.Wall(j * 2 + 1, i * 2);
+			var cell_left = this.cells[i][j];
+			var cell_right = this.cells[i][j + 1];
+			cell_left.neighbors['right'] = cell_right;
+			cell_left.neighbor_walls['right'] = wall;
+			cell_right.neighbors['left'] = cell_left;
+			cell_right.neighbor_walls['left'] = wall;
 		}
-	};
-	
-	/* Canvas used for drawing over the screen.
-	 */
-	maze.Canvas = function (element) {
-		this.context_2d = element.getContext("2d");
-		this.columns = Math.floor(element.width / maze.CELL_SIZE);
-		this.rows = Math.floor(element.height / maze.CELL_SIZE);
-		if (this.columns % 2 == 0) this.columns--;
-		if (this.rows % 2 == 0) this.rows--;
-		this.x_span = (element.width - maze.CELL_SIZE * this.columns) / 2;
-		this.y_span = (element.height - maze.CELL_SIZE * this.rows) / 2;
-		this.width = element.width;
-		this.height = element.height;
-		this.maze_rows = (this.rows -1)/2;
-		this.maze_columns = (this.columns -1)/2;
-		// Is this used?
-		if (this.maze_rows * this.maze_columns > 1)
-console.log('creating the maze structure');
-			this.structure = new maze.Maze(this.maze_rows, this.maze_columns);
-	};
-	maze.Canvas.prototype.draw = function () {
-		if (!this.structure.complete)
-			this.structure.continue_generation();
-		var ctx = this.context_2d; // Alias.
-		ctx.fillStyle="#000000";
-		ctx.fillRect(0, 0, this.width, this.height);
-		// Paint the cells.
-		for (i = 0; i < this.maze_rows; i++) {
-			for (j = 0; j < this.maze_columns; j++) {
-				// Note j is used for the x coordinate and i for the y.
-				var x = this.x_span + maze.CELL_SIZE * j * 2 + maze.CELL_SIZE;
-				var y = this.y_span + maze.CELL_SIZE * i * 2 + maze.CELL_SIZE;
-				ctx.fillStyle = (this.structure.cells[i][j].visited ? '#FFFFFF' : '#000000');
-				ctx.fillRect(x, y, maze.CELL_SIZE, maze.CELL_SIZE);
-			}
+	}
+	var i = Math.floor(Math.random() * this.rows);
+	var j = Math.floor(Math.random() * this.columns);
+	this.cell_stack.push(this.cells[i][j]);
+
+	this.ctx.fillStyle = maze.BACKGROUND;
+	this.ctx.fillRect(0, 0, element.width, element.height);
+	this.ctx.fillStyle = maze.FOREGROUND;
+};
+
+maze.Canvas.prototype.draw = function (object) {
+	var x = this.x_span + this.cell_size * (object.x + 1);
+	var y = this.y_span + this.cell_size * (object.y + 1);
+	this.ctx.fillRect(x, y, this.cell_size, this.cell_size);
+}
+
+maze.Canvas.prototype.update = function () {
+	var current_cell = this.cell_stack[this.cell_stack.length -1];
+	current_cell.visited = true;
+	this.draw(current_cell);
+	while (this.cell_stack.length > 0) {
+		var neighbors = current_cell.unvisited_neighbors();
+		var neighbors_count = 0;
+		var neighbors_list = [];
+		for (var key in neighbors) {
+			neighbors_count++;
+			neighbors_list.push(key);
 		}
-		// Paint the horizontal walls.
-		for (i = 0; i < this.maze_rows; i++) {
-			for (j = 0; j < this.maze_columns -1; j++) {
-				var wall = this.structure.cells[i][j].neighbor_walls['right'];
-				// Note j is used for the x coordinate and i for the y.
-				var x = this.x_span + maze.CELL_SIZE * j * 2 + maze.CELL_SIZE * 2;
-				var y = this.y_span + maze.CELL_SIZE * i * 2 + maze.CELL_SIZE;
-				ctx.fillStyle = (wall.exists ? '#000000' : '#FFFFFF');
-				ctx.fillRect(x, y, maze.CELL_SIZE, maze.CELL_SIZE);
-			}
+		if (neighbors_count > 0) {
+			// Chose a random neighbor and insert it into the stack.
+			var i = Math.floor(Math.random() * neighbors_count);
+			var direction = neighbors_list[i];
+			var wall = current_cell.neighbor_walls[direction];
+			this.draw(wall);
+			//wall.exists = false;
+			this.cell_stack.push(current_cell.neighbors[direction]);
+			break;
+		} else {
+			this.cell_stack.pop();
+			current_cell = this.cell_stack[this.cell_stack.length -1];
 		}
-		// Paint the vertical walls.
-		for (i = 0; i < this.maze_rows -1; i++) {
-			for (j = 0; j < this.maze_columns; j++) {
-				var wall = this.structure.cells[i][j].neighbor_walls['down'];
-				// Note j is used for the x coordinate and i for the y.
-				var x = this.x_span + maze.CELL_SIZE * j * 2 + maze.CELL_SIZE;
-				var y = this.y_span + maze.CELL_SIZE * i * 2 + maze.CELL_SIZE * 2;
-				ctx.fillStyle = (wall.exists ? '#000000' : '#FFFFFF');
-				ctx.fillRect(x, y, maze.CELL_SIZE, maze.CELL_SIZE);
-			}
-		}
-	};
-console.log('goodbye maze.js');
-//})();
+	}
+	if (this.cell_stack.length == 0)
+		this.complete = true;
+};
+
+})();
